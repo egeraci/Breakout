a157f6448ffa48a378407debaa04cfa02ff37f18
Elizabeth Geraci <egeraci@bsd220.org>
2021-04-01 12:16:09 -0500

    Made Background

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,7 +11,25 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        createBackground()
         
+    }
+    
+    func createBackground()
+    {
+        let stars = SKTexture(imageNamed: "Stars")
+        for i in 0...1
+        {
+            let starsbackground = SKSpriteNode (texture: stars)
+            starsbackground.zPosition = -1
+            starsbackground.position = CGPoint(x: 0, y: starsbackground.size.height * CGFloat(i))
+            addChild(starsbackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -starsbackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsbackground.size.height, duration: 0)
+            let moveloup = SKAction.sequence([moveDown, moveReset])
+            let moveforever = SKAction .repeatForever(moveloup)
+            starsbackground.run(moveforever)
+        }
     }
 }
 
