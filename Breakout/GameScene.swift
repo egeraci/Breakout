2e8eb0d36c85219a248331f915caeaae4252056a
Elizabeth Geraci <egeraci@bsd220.org>
2021-05-18 16:00:30 -0500

    DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -45,7 +45,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     func kickBall()
     {
         ball.physicsBody?.isDynamic = true
-        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
+        ball.physicsBody?.applyImpulse(CGVector(dx: Int.random(in: -5...5), dy: 5))
     }
     
     func createBackground()
@@ -218,9 +218,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate
                 contact.bodyB.node == brick
             {
              score += 1
-                //increase ball velocity by 2%
-                ball.physicsBody!.velocity.dx = ball.physicsBody!.velocity.dx
-                    * CGFloat(1.02)
+                // increase ball velocity by 2%
+                               ball.physicsBody!.velocity.dx = ball.physicsBody!.velocity.dx * CGFloat(1.02)
+                               ball.physicsBody!.velocity.dy = ball.physicsBody!.velocity.dy * CGFloat(1.02)
                 updateLabels()
                 if brick.color == .blue
                 {
